187ed7d6c14d80ad53684cf08a36ab157788074f
CoolzGH <knyazke@gmail.com>
2020-12-15 12:06:43 +0300

    Task 1

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -5,10 +5,25 @@ namespace CourseApp.Tests
 {
     public class DemoTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(10, 20, 30)]
+        [InlineData(-10, 20, 10)]
+        public void TestCalc(int aValue, int bValue, int exp)
         {
-            Assert.True(true);
+            Digit a = new Digit();
+            a.Value = aValue;
+            Digit b = new Digit();
+            b.Value = bValue;
+            var sum = Program.Calc(a, b);
+            Assert.Equal(exp, sum);
+        }
+
+        [Fact]
+        public void TestCalcWithNull()
+        {
+            Digit a = null;
+            Digit b = null;
+            Assert.Throws<ArgumentNullException>(() => Program.Calc(a, b));
         }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,9 +4,26 @@ namespace CourseApp
 {
     public class Program
     {
+        public static double Calc(Digit a, Digit b)
+        {
+            if ((a == null) || (b == null))
+            {
+                throw new ArgumentNullException("arrgs cannot be null");
+            }
+            else
+            {
+                var sum = a.Value + b.Value;
+                return sum;
+            }
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Digit a = new Digit();
+            a.Value = 10;
+            Digit b = new Digit();
+            b.Value = 20;
+            Console.WriteLine(Program.Calc(a, b));
             Console.ReadLine();
         }
     }
