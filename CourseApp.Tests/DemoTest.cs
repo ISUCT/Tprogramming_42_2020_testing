b1a78b218b31ced90aebd64765905f7b289bc5fc
etogjesanya <Alexander.Al-M@yandex.ru>
2020-12-17 14:17:44 +0300

    Task 1

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -5,10 +5,27 @@ namespace CourseApp.Tests
 {
     public class DemoTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(5, 5, 10)]
+        [InlineData(-5, -5, -10)]
+        [InlineData(-5, 5, 0)]
+        public void TestCalc(int x, int y, int exp)
         {
-            Assert.True(true);
+            Numer a = new Numer();
+            a.Value = x;
+            Numer b = new Numer();
+            b.Value = y;
+            Assert.Equal(exp, Program.Calc(a, b));
+        }
+
+        [Fact]
+        public void TestForNull()
+        {
+            Numer a = new Numer();
+            Numer b = new Numer();
+            a.Value = null;
+            b.Value = null;
+            Assert.Throws<System.NullReferenceException>(() => Program.Calc(a, b));
         }
     }
 }
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,9 +4,26 @@ namespace CourseApp
 {
     public class Program
     {
+        public static int Calc(Numer a, Numer b)
+        {
+            if ((a == null) || (b == null))
+            {
+                throw new System.NullReferenceException("a or b");
+            }
+            else
+            {
+                int c = (int)a.Value + (int)b.Value;
+                return c;
+            }
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Numer a = new Numer();
+            Numer b = new Numer();
+            a.Value = 5;
+            b.Value = 5;
+            Console.WriteLine(Program.Calc(a, b));
             Console.ReadLine();
         }
     }
